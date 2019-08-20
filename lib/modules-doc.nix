e481f5b96e5182234858efc3ce62a350674abee9
Robert Helgesson <robert@rycee.net>
2019-08-20 20:12:15 +0200

    Apply scrubbing of the package set given to the modules
    This is copied over, with some minor cleanups, from the NixOS
    documentation framework.

--- lib/modules-doc.nix
+++ lib/modules-doc.nix
@@ -26,7 +26,49 @@ with lib;
 
 let
 
-  evaluatedModules = evalModules { inherit modules; };
+  # Recursively replace each derivation in the given attribute set
+  # with the same derivation but with the `outPath` attribute set to
+  # the string `"\${pkgs.attribute.path}"`. This allows the
+  # documentation to refer to derivations through their values without
+  # establishing an actual dependency on the derivation output.
+  #
+  # This is not perfect, but it seems to cover a vast majority of use
+  # cases.
+  #
+  # Caveat: even if the package is reached by a different means, the
+  # path above will be shown and not e.g.
+  # `${config.services.foo.package}`.
+  scrubDerivations = prefixPath: attrs:
+    let
+      scrubDerivation = name: value:
+        let
+          pkgAttrName = prefixPath + "." + name;
+        in
+          if isAttrs value then
+            scrubDerivations pkgAttrName value
+            // optionalAttrs (isDerivation value) {
+              outPath = "\${${pkgAttrName}}";
+            }
+          else
+            value;
+    in
+      mapAttrs scrubDerivation attrs;
+
+  # Evaluate the modules on a scrubbed package set.
+  evaluatedModules =
+    let
+      scrubbedPkgsModule = {
+        imports = [
+          {
+            _module.args = {
+              pkgs = mkForce (scrubDerivations "pkgs" pkgs);
+              pkgs_i686 = mkForce { };
+            };
+          }
+        ];
+      };
+    in
+      evalModules { modules = modules ++ [ scrubbedPkgsModule ]; };
 
   optionsDocs =
     (map cleanUpOption
