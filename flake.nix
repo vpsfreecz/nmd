824a380546b5d0d0eb701ff8cd5dbafb360750ff
Robert Helgesson <robert@rycee.net>
2023-06-24 19:23:48 +0200

    Rework Nix Flake file a bit
    Also add basic sourcehut build specification that just checks the
    source formatting.

--- flake.nix
+++ flake.nix
@@ -11,87 +11,108 @@
 
   outputs = { self, nixpkgs, scss-reset }:
     let
-      supportedSystems = nixpkgs.lib.platforms.unix;
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      lib = nixpkgs.lib;
+
+      forAllSystems = lib.genAttrs supportedSystems;
+
+      flakePkgs = pkgs:
+        let
+          fpkgs = {
+            generateCss = pkgs.writeShellScript "generateCss" ''
+              set -euo pipefail
+
+              tmpfile=$(mktemp -d)
+              trap "rm -r $tmpfile" EXIT
+
+              ln -s "${scss-reset}/build" "$tmpfile/scss-reset"
+
+              PATH=${lib.makeBinPath [ pkgs.coreutils pkgs.rsass ]}
+
+              rsass \
+                --load-path="$tmpfile" \
+                --style compressed \
+                ./static/style.scss \
+                > ./static/style.css
+              echo "Generated ./static/style.css"
+            '';
+
+            # Very basic script to re-start the server and perform Tailwind CSS
+            # export on file changes.
+            p-watch = pkgs.writeShellScriptBin "p-watch" ''
+              set -euo pipefail
+
+              trap "kill 0" EXIT
+
+              watchArgs=""
+              while (( $# > 0 )); do
+                opt="$1"
+                shift
+                case $opt in
+                  --notify)
+                    watchArgs="$watchArgs -N"
+                    ;;
+                  *)
+                    echo "Unknown argument: $opt" >&2
+                    exit 1
+                    ;;
+                esac
+              done
+
+              ${pkgs.watchexec}/bin/watchexec $watchArgs \
+                -e scss \
+                -- ${fpkgs.generateCss}
+            '';
+
+            p-build = pkgs.writeShellScriptBin "p-build" ''
+              exec ${fpkgs.generateCss}
+            '';
+
+            p-format = pkgs.writeShellScriptBin "p-format" ''
+              shopt -s globstar
+              set -euo pipefail
+
+              case ''${1:-} in
+                -h) echo "$0 [-c]" ;;
+                -c) check=""       ;;
+              esac
+
+              PATH=${with pkgs; lib.makeBinPath [ nixfmt yapf ]}
+
+              nixfmt ''${check+-c} **/*.nix
+              yapf ''${check--i} ''${check+-d} **/*.py
+            '';
+          };
+        in fpkgs;
     in {
       lib = forAllSystems (system:
         import ./default.nix { pkgs = nixpkgs.legacyPackages.${system}; });
 
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          fpkgs = flakePkgs pkgs;
+        in { inherit (fpkgs) p-format; });
+
       devShells = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          lib = pkgs.lib;
-
-          generateCss = pkgs.writeShellScript "generateCss" ''
-            set -euo pipefail
-
-            tmpfile=$(mktemp -d)
-            trap "rm -r $tmpfile" EXIT
-
-            ln -s "${scss-reset}/build" "$tmpfile/scss-reset"
-
-            PATH=${lib.makeBinPath [ pkgs.coreutils pkgs.rsass ]}
-
-            rsass \
-              --load-path="$tmpfile" \
-              --style compressed \
-              ./static/style.scss \
-              > ./static/style.css
-            echo "Generated ./static/style.css"
-          '';
-
-          # Very basic script to re-start the server and perform Tailwind CSS
-          # export on file changes.
-          pWatch = pkgs.writeShellScriptBin "p-watch" ''
-            set -euo pipefail
-
-            trap "kill 0" EXIT
-
-            watchArgs=""
-            while (( $# > 0 )); do
-              opt="$1"
-              shift
-              case $opt in
-                --notify)
-                  watchArgs="$watchArgs -N"
-                  ;;
-                *)
-                  echo "Unknown argument: $opt" >&2
-                  exit 1
-                  ;;
-              esac
-            done
-
-            ${pkgs.watchexec}/bin/watchexec $watchArgs \
-              -e scss \
-              -- ${generateCss}
-          '';
-
-          pBuild = pkgs.writeShellScriptBin "p-build" ''
-            exec ${generateCss}
-          '';
-
-          pFormat = pkgs.writeShellScriptBin "p-format" ''
-            shopt -s globstar
-            set -euo pipefail
-
-            case ''${1:-} in
-              -h) echo "$0 [-c]" ;;
-              -c) check=""       ;;
-            esac
-
-            PATH=${with pkgs; nixpkgs.lib.makeBinPath [ nixfmt yapf ]}
-
-            nixfmt ''${check+-c} **/*.nix
-            yapf ''${check--i} ''${check+-d} **/*.py
-          '';
+          fpkgs = flakePkgs pkgs;
         in {
           default = pkgs.mkShell {
             name = "dev-shell";
             nativeBuildInputs = [
-              pFormat
-              pWatch
-              pBuild
+              fpkgs.p-format
+              fpkgs.p-watch
+              fpkgs.p-build
+
               pkgs.asciidoc
               (pkgs.python3.withPackages (p: [ p.mistune ]))
 
